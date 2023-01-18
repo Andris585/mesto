25361f071cb44a23d096e565dd3238b0b55495f7
585Andris <morbiddesire17@yandex.ru>
2023-01-18 21:55:24 +0300

    feat: сделать скрипты

---
+++ script/index.js
@@ -0,0 +1,37 @@
+const profileElement = document.querySelector('.profile');
+const profileNameElement = profileElement.querySelector('.profile__name');
+const profileBioElement = profileElement.querySelector('.profile__bio');
+const profileEditButtonElement = profileElement.querySelector('.profile__button-edit');
+const profileAddButtonElement = profileElement.querySelector('.profile__button-add');
+const popupElement = document.querySelector('.popup');
+const popupFormElement = popupElement.querySelector('.popup__form');
+const popupInputNameElement = popupElement.querySelector('.popup__input-name');
+const popupInputBioElement = popupElement.querySelector('.popup__input-bio');
+const popupCloseButtonElement = popupElement.querySelector('.popup__close-button');
+const popupSubmitButtonElement = popupElement.querySelector('.popup__submit');
+const elementsLikeButtonElement = document.querySelector('.elements__like');
+
+
+
+const openPopup = function() {
+  popupElement.classList.add('popup_opened');
+  popupInputNameElement.value = profileNameElement.textContent;
+  popupInputBioElement.value = profileBioElement.textContent;
+}
+
+const closePopup = function() {
+  popupElement.classList.remove('popup_opened');
+}
+
+profileEditButtonElement.addEventListener('click', openPopup);
+popupCloseButtonElement.addEventListener('click', closePopup)
+
+function handleFormSubmit (evt) {
+    evt.preventDefault(); 
+    let profileNameEdited = popupInputNameElement.value;
+    let profileBioEdited = popupInputBioElement.value;
+    profileNameElement.textContent = profileNameEdited;
+    profileBioElement.textContent = profileBioEdited;
+}
+
+popupFormElement.addEventListener('submit', handleFormSubmit);
